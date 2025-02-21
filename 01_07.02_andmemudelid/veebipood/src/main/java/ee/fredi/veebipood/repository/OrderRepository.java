11657799923fdd89544e81feb7d8aec6c3c78a7e
fredtare <tarenomm@tlu.ee>
2025-02-21 18:08:52 +0200

    Revert "Tund 2 ja 3 uuendused ja kodutöö algus."
    This reverts commit 4ebe960b6e6f498c8b35a45b15215b2327c419a1.

--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/fredi/veebipood/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package ee.fredi.veebipood.repository;
-
-
-import ee.fredi.veebipood.entity.WebshopOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<WebshopOrder, Long> {
-}
--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/fredi/veebipood/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.fredi.veebipood.repository;
-
-import ee.fredi.veebipood.entity.WebshopUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<WebshopUser, Long> {
-}
